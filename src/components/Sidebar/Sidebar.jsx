ff14b9bc405b928eb0f43191eab914ee15558ef3
shagors <shajjadshagor@gmail.com>
2023-05-25 09:14:51 -0700

    design and set navbar and add search bar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -8,7 +8,9 @@ const Sidebar = () => {
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <h2><i className='ri-taxi-line'></i> CarX</h2>
+                <h2>
+                    <span><i className='ri-taxi-line'></i></span> CarX
+                </h2>
             </div>
 
             <div className="sidebar__content">
@@ -16,14 +18,20 @@ const Sidebar = () => {
                     <ul className="nav__list">
                         {
                             navLinks?.map((item, index) => (
-                                <li className="nav__item">
-                                    <NavLink to={item.path}>
-                                        <span><i className={item.icon}></i></span> {item.display}
+                                <li className="nav__item" key={index}>
+                                    <NavLink to={item.path} className={navClass => navClass.isActive ? "nav__active nav__link" : "nav__link"}>
+                                        <i className={item.icon}></i> {item.display}
                                     </NavLink>
                                 </li>
                             ))
                         }
                     </ul>
+                </div>
+
+                <div className="sidebar__bottom">
+                    <span>
+                        <i className='ri-logout-circle-r-line'></i> Logout
+                    </span>
                 </div>
             </div>
         </div>
