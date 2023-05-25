8dcbb2a0177ab1679b6afa1300a6666d67bc6550
shagors <shajjadshagor@gmail.com>
2023-05-25 05:25:47 -0700

    sidebar design and modify it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Layout />
     </div>
   );
 }
--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Bookings from '../pages/Bookings';
 import SellCar from '../pages/SellCar';
@@ -8,14 +8,13 @@ import Settings from '../pages/Settings';
 
 const Router = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/dashboard' element={<Dashboard />} />
-                <Route path='/bookings' element={<Bookings />} />
-                <Route path='/sell-car' element={<SellCar />} />
-                <Route path='/settings' element={<Settings />} />
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Navigate to="/dashboard" element={<Dashboard />} />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/bookings' element={<Bookings />} />
+            <Route path='/sell-car' element={<SellCar />} />
+            <Route path='/settings' element={<Settings />} />
+        </Routes>
     )
 }
 
