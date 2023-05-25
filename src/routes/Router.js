1e1d1e379fa20a1936530f51ab1c25471925e175
shagors <shajjadshagor@gmail.com>
2023-05-25 04:12:42 -0700

    router set and set all pages

---
+++ src/routes/Router.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard';
+import Bookings from '../pages/Bookings';
+import SellCar from '../pages/SellCar';
+import Settings from '../pages/Settings';
+
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/bookings' element={<Bookings />} />
+                <Route path='/sell-car' element={<SellCar />} />
+                <Route path='/settings' element={<Settings />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Router
