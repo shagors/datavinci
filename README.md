4390a7c01064dcf6e09aefcb14584c3a46dd7f41
shagors <shajjadshagor@gmail.com>
2023-05-26 06:18:25 -0700

    form design and complete design

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## What I use for development
 
-
-
 ![preview img](/previewImg/Capture1.PNG)
+![preview img](/previewImg/Capture2.PNG)
+![preview img](/previewImg/Capture3.PNG)
+![preview img](/previewImg/Capture4.PNG)
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from "react";
+import "../styles/settings.css";
 
 const Settings = () => {
-    return (
-        <div>Settings</div>
-    )
-}
+  return (
+    <div className="settings">
+      <div className="settings__wrapper">
+        <h2 className="settings__title">Settings</h2>
 
-export default Settings
+        <div className="settings__top">
+          <button className="setting__btn">My Details</button>
+          <button className="setting__btn active__btn">Profile</button>
+          <button className="setting__btn">Password</button>
+          <button className="setting__btn">Email</button>
+          <button className="setting__btn">Notification</button>
+        </div>
+
+        <div className="details__form">
+          <h2 className="profile__title">Profile</h2>
+          <p className="profile__desc">
+            Update your photo and personal details here
+          </p>
+          <form>
+            <div className="form__group">
+              <div>
+                <label>Live in</label>
+                <input type="text" placeholder="Rangpur, Bangladesh" />
+              </div>
+
+              <div>
+                <label>Street</label>
+                <input type="text" placeholder="JL RAY ROAD" />
+              </div>
+            </div>
+
+            <div className="form__group">
+              <div>
+                <label>Email</label>
+                <input type="email" placeholder="shajjadshagor@gmail.com" />
+              </div>
+
+              <div>
+                <label>Phone Number</label>
+                <input type="number" placeholder="+880 17*******" />
+              </div>
+            </div>
+
+            <div className="form__group">
+              <div>
+                <label>Date of Birth</label>
+                <input type="date" placeholder="dd/mm/yyyy" />
+              </div>
+
+              <div>
+                <label>Gender</label>
+                <input type="text" placeholder="Male" />
+              </div>
+            </div>
+
+            <div className="form__group">
+              <div>
+                <label>Your Photo</label>
+                <p className="profile-img__desc">
+                  This will be displayed in your profile
+                </p>
+                <input type="file" placeholder="choose file" />
+              </div>
+
+              <div className="profile__img-btns">
+                <button className="dlt__btn">Delete</button>
+                <button className="update__btn">Update</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
